32f5e04068cd56a8bb102efc1545446b4defe517
donaldc <donaldc354@gmail.com>
2019-04-12 16:08:59 -0400

    commit

--- randomMaze.py
+++ randomMaze.py
@@ -125,19 +125,14 @@ def setSpeedsIPS(ipsLeft, ipsRight):
       lPwmValue = float(LWSpeed[rpsLeft])
       rPwmValue = float(RWSpeed[rpsRight])
 
-      if ipsLeft < 0 and ipsRight < 0:
+      if ipsLeft < 0 or ipsRight < 0:
             # Setting appropiate speeds to the servos when going forwards
             pwm.set_pwm(LSERVO, 0, math.floor(lPwmValue / 20 * 4096))
             pwm.set_pwm(RSERVO, 0, math.floor(setDifference(rPwmValue) / 20 * 4096))
-      elif ipsLeft >= 0 and ipsRight >= 0:
+      elif ipsLeft >= 0 or ipsRight >= 0:
             # Setting apporpiate speeds to the servos when going backwards
             pwm.set_pwm(LSERVO, 0, math.floor(setDifference(lPwmValue) / 20 * 4096))
             pwm.set_pwm(RSERVO, 0, math.floor(rPwmValue / 20 * 4096))
-      elif ipsLeft >= 0 and ipsRight < 0:
-            # Setting appropriate speedsto the servos while making a turn
-            pwm.set_pwm(LSERVO, 0, math.floor(setDifference(lPwmValue) / 20 * 4096))
-            pwm.set_pwm(LSERVO, 0, math.floor(setDifference(rPwmValue) / 20 * 4096))
-
 def spinIPS(ipsLeft, ipsRight):
       # Converting inches per second into revolutions per second
       rpsLeft = float(math.ceil((ipsLeft / 8.20) * 100) / 100)
